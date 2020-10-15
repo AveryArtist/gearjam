48fc36b83590d57eb82e1c05574ee8d2be99346c
Avery Pudding <67764012+AveryArtist@users.noreply.github.com>
2020-10-15 18:38:26 -0300

    modificações no gamecontroller

--- gearjam/Assets/Scripts/GameController.cs
+++ gearjam/Assets/Scripts/GameController.cs
@@ -5,5 +5,25 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
+    private int position;
+    private int speed;
+
+    public Text velocityText;
+    public Text positionText;
+
+    void Start(){
+        position = PlayerPrefs.GetInt("position", 8);
+        BeginGame();
+    }
+
+    void Update(){
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void BeginGame(){
+        speed = 0;
+
+        velocityText.text = speed;
+    }
 }
