e5aa21f769e96f391f1cba0c560d8c1e78bf8fc0
Avery Pudding <67764012+AveryArtist@users.noreply.github.com>
2020-10-15 18:14:20 -0300

    Criei um script pro cenario e jogo, e um script pro jogador

---
+++ gearjam/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @Triloso Gabe esse daqui é o script pra programar o jogador
+public class PlayerController : MonoBehaviour
+{
+    //no caso tu programa aqui dentro
+
+
+
+}
