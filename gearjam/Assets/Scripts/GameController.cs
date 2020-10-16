e9b3c3d3d714dd46bfc76dcbfef762d07981179d
Avery Pudding <67764012+AveryArtist@users.noreply.github.com>
2020-10-15 23:07:00 -0300

    função de aceleração adicionada (não testada)

--- gearjam/Assets/Scripts/GameController.cs
+++ gearjam/Assets/Scripts/GameController.cs
@@ -8,6 +8,11 @@ public class GameController : MonoBehaviour
     private int position;
     private int speed;
 
+    private float initialVelocity = 0.0;
+    private float finalVelocity = 500.0;
+    private float accelerationRate = 10.0;
+    private float decelerationRate = 50.0;
+
     public Text velocityText;
     public Text positionText;
 
@@ -26,4 +31,19 @@ public class GameController : MonoBehaviour
 
         velocityText.text = speed;
     }
+
+    void accelerate(){
+        if(Input.GetKeyDown(UpArrow)){
+            currentVelocity = currentVelocity + (accelerationRate * Time.deltaTime);
+        }
+        else{
+            currentVelocity = currentVelocity - (decelerationRate * Time.deltaTime);
+        }
+
+        currentVelocity = Mathf.Clamp(currentVelocity, initialVelocity, finalVelocity);
+
+        transform.Translate(0,0,power);
+    }
+
+    
 }
