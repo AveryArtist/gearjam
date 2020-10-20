ffab18706d3c601f51474afc7dcb3e05a2c49d86
Avery Pudding <67764012+AveryArtist@users.noreply.github.com>
2020-10-20 15:19:12 -0300

    final das corridas, posicionamento

--- gearjam/Assets/Scripts/GameController.cs
+++ gearjam/Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject playerObject;
+
     private int position;
     private int speed;
     private float power = 500.0;
@@ -19,15 +21,19 @@ public class GameController : MonoBehaviour
     public Text velocityText;
     public Text positionText;
     public Text countdownText;
+    public Text winText;
 
-    void Start(){
+    void Start(){ //pegar a posição do jogador, ter que colocar ja posições para todos os oponentes, e na pista tbm
         position = PlayerPrefs.GetInt("position", 8);
         BeginGame();
     }
 
     void Update(){
-        if (Input.GetKey("escape"))
+        if (Input.GetKey("escape")){
             Application.Quit();
+        }
+
+        Position();
     }
 
     void BeginGame(){
@@ -39,7 +45,7 @@ public class GameController : MonoBehaviour
   
     }
 
-    void accelerate(){
+    void accelerate(){ //função de aceleração, ta funcionando, só precisa adicionar como isso vai fazer efeito com o cenario
         if(Input.GetKeyDown(UpArrow)){
             currentVelocity = currentVelocity + (accelerationRate * Time.deltaTime);
         }
@@ -52,7 +58,7 @@ public class GameController : MonoBehaviour
         transform.Translate(0,0,power);
     }
 
-    void CountDown(){
+    void CountDown(){ //contador de 3 a 0, e depois GO pra começar a corrida, coloquei ja na variavel de countdownText, pra ficar mais facil de mudar na UI
 
         for(int i = 0; i < countdown;i++){
             countdownText.text = i;
@@ -61,5 +67,19 @@ public class GameController : MonoBehaviour
         countdownText.text = "GO";
     }
 
-    
+    void Position(){
+        if (playerObject.position > opponentObject.position){
+            position--;
+        } else if(opponentObject.position > playerObject.position){
+            position++;
+        }
+    }
+
+    void end(){ //função para o final da corrida, determinar se o jogador venceu ou não (ainda a programar posições)
+        if(positionText <= 3){
+            winText = "You Win!";
+        } else if(positionText > 3){
+            winText = "You Lose";
+        }
+    }
 }
--- gearjam/Assets/Scripts/playercontroller.cs
+++ gearjam/Assets/Scripts/playercontroller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 // @Triloso Gabe esse daqui é o script pra programar o jogador
 public class PlayerController : MonoBehaviour
 {
+    private GameController gameController; //puxando o meu script pra cá, ai da pra tu puxar minhas funções
     //no caso tu programa aqui dentro
 
 
